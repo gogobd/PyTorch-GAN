6af138eb9a5d293ca134a3e5c66cbfd3aeef3e98
Georg Bernhard <gogo@bluedynamics.com>
2020-01-04 16:55:48 +0100

    Don't remove docker container after stop, cleanup.

--- package.sh
+++ package.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 rm package.tgz
-tar --exclude '._*' --exclude '__pycache__' --exclude 'saved_models' --exclude 'images' --exclude './data' --exclude './implementations/cyclegan/core' -zcvf package.tgz ./*
+find . -name '._*' -exec rm {} \;
+tar --exclude '__pycache__' --exclude 'saved_models' --exclude 'images' --exclude './data' --exclude './implementations/cyclegan/core' -zcvf package.tgz ./*
