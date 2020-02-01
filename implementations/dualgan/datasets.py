57ea30a0e411756ae60b31bbba3ce5295dd5e4b3
root <gogo@bluedynamics.com>
2020-02-01 20:15:33 +0000

    Generating pixellated image pairs, allowed for optimizer reload

--- implementations/dualgan/datasets.py
+++ implementations/dualgan/datasets.py
@@ -11,7 +11,6 @@ import torchvision.transforms as transforms
 class ImageDataset(Dataset):
     def __init__(self, root, transforms_=None, mode="train"):
         self.transform = transforms.Compose(transforms_)
-
         self.files = sorted(glob.glob(os.path.join(root, mode) + "/*.*"))
 
     def __getitem__(self, index):
@@ -24,6 +23,53 @@ class ImageDataset(Dataset):
         if np.random.random() < 0.5:
             img_A = Image.fromarray(np.array(img_A)[:, ::-1, :], "RGB")
             img_B = Image.fromarray(np.array(img_B)[:, ::-1, :], "RGB")
+
+        img_A = self.transform(img_A)
+        img_B = self.transform(img_B)
+
+        return {"A": img_A, "B": img_B}
+
+    def __len__(self):
+        return len(self.files)
+
+class ImageDataset_Pixellated(Dataset):
+    def __init__(self, root, transforms_=None, random_transforms_=None, mode="train"):
+        self.transform = transforms.Compose(transforms_)
+        self.random_transform = transforms.Compose(random_transforms_)
+        self.files = sorted(glob.glob(os.path.join(root % mode, "*.jpg")))
+
+    def __getitem__(self, index):
+
+        img = Image.open(self.files[index % len(self.files)])
+        # Convert grayscale images to rgb
+        if img.mode != "RGB":
+            img = to_rgb(img)
+        w, h = img.size
+        img = self.random_transform(img)
+        img_A = img.copy()
+        img_B = img.copy()
+        img_M = img.copy()
+
+        # Paste random patch
+        patch_size_w = random.randrange(16, w)
+        patch_size_h = random.randrange(16, h)
+        x1 = random.randint(1, w-patch_size_w)
+        y1 = random.randint(1, h-patch_size_h)
+        x2 = x1+patch_size_w
+        y2 = y1+patch_size_h
+        # print(x1,y1,x2,y2)
+        img_M = img_M.crop((x1, y1, x2, y2))
+        # Create pixellated image
+        pixel_size = random.randint(2, 64)
+        img_M = img_M.resize(
+            (
+                patch_size_w//pixel_size + 1,
+                patch_size_h//pixel_size + 1,
+            ),
+            resample=Image.BILINEAR
+        )
+        img_M = img_M.resize((patch_size_w, patch_size_h), Image.NEAREST)
+        img_B.paste(img_M, (x1, y1))
 
         img_A = self.transform(img_A)
         img_B = self.transform(img_B)
--- implementations/dualgan/dualgan.py
+++ implementations/dualgan/dualgan.py
@@ -28,6 +28,7 @@ os.makedirs("images", exist_ok=True)
 parser = argparse.ArgumentParser()
 parser.add_argument("--epoch", type=int, default=0, help="epoch to start training from")
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--load_model", type=str, default='', help="model to load (format: epoch_batch)")
 parser.add_argument("--batch_size", type=int, default=8, help="size of the batches")
 parser.add_argument("--dataset_name", type=str, default="edges2shoes", help="name of the dataset")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
@@ -70,12 +71,22 @@ if cuda:
     D_B.cuda()
     cycle_loss.cuda()
 
-if opt.epoch != 0:
+# Optimizers
+optimizer_G = torch.optim.Adam(
+    itertools.chain(G_AB.parameters(), G_BA.parameters()), lr=opt.lr, betas=(opt.b1, opt.b2)
+)
+optimizer_D_A = torch.optim.Adam(D_A.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D_B = torch.optim.Adam(D_B.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+if opt.load_model != '':
     # Load pretrained models
-    G_AB.load_state_dict(torch.load("saved_models/%s/G_AB_%d.pth" % (opt.dataset_name, opt.epoch)))
-    G_BA.load_state_dict(torch.load("saved_models/%s/G_BA_%d.pth" % (opt.dataset_name, opt.epoch)))
-    D_A.load_state_dict(torch.load("saved_models/%s/D_A_%d.pth" % (opt.dataset_name, opt.epoch)))
-    D_B.load_state_dict(torch.load("saved_models/%s/D_B_%d.pth" % (opt.dataset_name, opt.epoch)))
+    G_AB.load_state_dict(torch.load("saved_models/%s/G_AB_%d.pth" % (opt.dataset_name, load_model)))
+    G_BA.load_state_dict(torch.load("saved_models/%s/G_BA_%d.pth" % (opt.dataset_name, load_model)))
+    D_A.load_state_dict(torch.load("saved_models/%s/D_A_%d.pth" % (opt.dataset_name, load_model)))
+    D_B.load_state_dict(torch.load("saved_models/%s/D_B_%d.pth" % (opt.dataset_name, load_model)))
+    optimizer_G.load_state_dict(torch.load("saved_models/%s/optimizer_G_%s.pth" % (opt.dataset_name, opt.load_model)))
+    optimizer_D_A.load_state_dict(torch.load("saved_models/%s/optimizer_D_A_%s.pth" % (opt.dataset_name, opt.load_model)))
+    optimizer_D_B.load_state_dict(torch.load("saved_models/%s/optimizer_D_B_%s.pth" % (opt.dataset_name, opt.load_model)))
 else:
     # Initialize weights
     G_AB.apply(weights_init_normal)
@@ -83,31 +94,39 @@ else:
     D_A.apply(weights_init_normal)
     D_B.apply(weights_init_normal)
 
-# Configure data loader
+# Image transformations
+random_transforms_ = [
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),
+    transforms.ColorJitter(brightness=0.25, contrast=0.25, saturation=0.25, hue=0.1),
+]
 transforms_ = [
-    transforms.Resize((opt.img_size, opt.img_size), Image.BICUBIC),
+    transforms.Resize(int(opt.img_size), Image.BICUBIC),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 ]
+
 dataloader = DataLoader(
-    ImageDataset("../../data/%s" % opt.dataset_name, transforms_=transforms_),
+    ImageDataset_Pixellated(
+        "../../data/%s/%%s/A" % opt.dataset_name,
+        transforms_=transforms_,
+        random_transforms_=random_transforms_,
+    ),
     batch_size=opt.batch_size,
     shuffle=True,
     num_workers=opt.n_cpu,
 )
 val_dataloader = DataLoader(
-    ImageDataset("../../data/%s" % opt.dataset_name, mode="val", transforms_=transforms_),
-    batch_size=16,
+    ImageDataset_Pixellated(
+        "../../data/%s/%%s/A" % opt.dataset_name,
+        mode="test",
+        transforms_=transforms_,
+        random_transforms_=random_transforms_,
+    ),
+    batch_size=4,
     shuffle=True,
-    num_workers=1,
+    num_workers=opt.n_cpu,
 )
-
-# Optimizers
-optimizer_G = torch.optim.Adam(
-    itertools.chain(G_AB.parameters(), G_BA.parameters()), lr=opt.lr, betas=(opt.b1, opt.b2)
-)
-optimizer_D_A = torch.optim.Adam(D_A.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-optimizer_D_B = torch.optim.Adam(D_B.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
 
 FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
@@ -140,12 +159,18 @@ def sample_images(batches_done):
     imgs = next(iter(val_dataloader))
     real_A = Variable(imgs["A"].type(FloatTensor))
     fake_B = G_AB(real_A)
-    AB = torch.cat((real_A.data, fake_B.data), -2)
     real_B = Variable(imgs["B"].type(FloatTensor))
     fake_A = G_BA(real_B)
+    AB = torch.cat((real_A.data, fake_B.data), -2)
     BA = torch.cat((real_B.data, fake_A.data), -2)
-    img_sample = torch.cat((AB, BA), 0)
+    img_sample = torch.cat((AB, BA), -2)
     save_image(img_sample, "images/%s/%s.png" % (opt.dataset_name, batches_done), nrow=8, normalize=True)
+
+if cuda:
+    print(torch.cuda.get_device_name(0))
+    print('Memory Usage:')
+    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
 
 
 # ----------
@@ -258,3 +283,6 @@ for epoch in range(opt.n_epochs):
         torch.save(G_BA.state_dict(), "saved_models/%s/G_BA_%d.pth" % (opt.dataset_name, epoch))
         torch.save(D_A.state_dict(), "saved_models/%s/D_A_%d.pth" % (opt.dataset_name, epoch))
         torch.save(D_B.state_dict(), "saved_models/%s/D_B_%d.pth" % (opt.dataset_name, epoch))
+        torch.save(optimizer_G.state_dict(), "saved_models/%s/optimizer_G_%d.pth" % (opt.dataset_name, epoch))
+        torch.save(optimizer_D_A.state_dict(), "saved_models/%s/optimizer_D_A_%d.pth" % (opt.dataset_name, epoch))
+        torch.save(optimizer_D_B.state_dict(), "saved_models/%s/optimizer_D_B_%d.pth" % (opt.dataset_name, epoch))
